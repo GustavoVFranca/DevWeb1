fadf1353fd277f77f4cbd843493fdfc14fb0d650
GustavoVFranca <gustavofranca7778@gmail.com>
2026-08-31 19:51:06 -0300

    cadastro pets

---
+++ devWeb 2/cadastro_pets/src/main/java/br/com/cadastropets/rest/JakartaRestConfiguration.java
@@ -0,0 +1,9 @@
+package br.com.cadastropets.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class JakartaRestConfiguration extends Application {
+
+}
